f5e9f27c0354b4478f3a1e6a0fad734b4bd722ab
rowland henshaw <rowland.henshaw@gmail.com>
2018-07-14 23:04:57 +0100

    saving to figure out how to inject IConfiguration service to use env

---
+++ Data/DmsContext.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using dotnetDMS.Models;
+
+namespace dotnetDMS.Data
+{
+    public partial class DmsContext : DbContext
+    {
+        public virtual DbSet<Documents> Documents { get; set; }
+        public virtual DbSet<Roles> Roles { get; set; }
+        public virtual DbSet<SequelizeMeta> SequelizeMeta { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(/*put connection stringh ere*/);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Documents>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Access)
+                    .IsRequired()
+                    .HasColumnName("access");
+
+                entity.Property(e => e.Content)
+                    .IsRequired()
+                    .HasColumnName("content");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnName("createdAt")
+                    .HasColumnType("timestamptz");
+
+                entity.Property(e => e.OwnerId).HasColumnName("ownerId");
+
+                entity.Property(e => e.OwnerRoleId).HasColumnName("ownerRoleId");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnName("title");
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnName("updatedAt")
+                    .HasColumnType("timestamptz");
+
+                entity.HasOne(d => d.Owner)
+                    .WithMany(p => p.Documents)
+                    .HasForeignKey(d => d.OwnerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Documents_ownerId_fkey");
+            });
+
+            modelBuilder.Entity<Roles>(entity =>
+            {
+                entity.HasIndex(e => e.Title)
+                    .HasName("Roles_title_key")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnName("createdAt")
+                    .HasColumnType("timestamptz");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnName("title");
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnName("updatedAt")
+                    .HasColumnType("timestamptz");
+            });
+
+            modelBuilder.Entity<SequelizeMeta>(entity =>
+            {
+                entity.HasKey(e => e.Name);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasIndex(e => e.Email)
+                    .HasName("Users_email_key")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnName("createdAt")
+                    .HasColumnType("timestamptz");
+
+                entity.Property(e => e.CurrentToken).HasColumnName("currentToken");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("firstName");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("lastName");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password");
+
+                entity.Property(e => e.RoleId).HasColumnName("roleId");
+
+                entity.Property(e => e.UpdatedAt)
+                    .HasColumnName("updatedAt")
+                    .HasColumnType("timestamptz");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Users)
+                    .HasForeignKey(d => d.RoleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Users_roleId_fkey");
+            });
+        }
+    }
+}
