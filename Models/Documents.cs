de10c0e09b3185a013e92767b6ce76117aa87cf9
rowland henshaw <rowland.henshaw@gmail.com>
2018-07-14 23:06:10 +0100

    created doc model

---
+++ Models/Documents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnetDMS.Models
+{
+    public partial class Documents
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int OwnerId { get; set; }
+        public int OwnerRoleId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Access { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Users Owner { get; set; }
+    }
+}
