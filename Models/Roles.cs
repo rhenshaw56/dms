3c7739c86139e43a1f0a1c52e4a8ba3c0d873e96
rowland henshaw <rowland.henshaw@gmail.com>
2018-07-14 23:06:31 +0100

    created role model

---
+++ Models/Roles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnetDMS.Models
+{
+    public partial class Roles
+    {
+        public Roles()
+        {
+            Users = new HashSet<Users>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<Users> Users { get; set; }
+    }
+}
